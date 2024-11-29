5302c7a8d24b5925966754c3c017307a985f6e77
Ganesh Raman <ganesh@Ganeshs-MacBook-Pro.local>
2024-11-28 22:28:35 -0800

    openai changes - WIP

--- routes/openai.ts
+++ routes/openai.ts
@@ -1,19 +1,25 @@
 import express from 'express';
 import dotenv from "dotenv";
 import axios from 'axios';
+import OpenAI from 'openai';
 
 dotenv.config();
 
-const OPENAI_URL = process.env.OPENAI_URL || "https://api.openai.com/v1/";
+const OPENAI_URL = process.env.OPENAI_URL || "https://api.openai.com/v1";
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 const OPENAI_ORGANIZATION = process.env.OPENAI_ORGANIZATION;
 const OPENAI_PROJECT = process.env.OPENAI_PROJECT;
 
+// Initialize the OpenAI client
+// const client = new OpenAI({
+//   apiKey: OPENAI_API_KEY, // This is the default and can be omitted
+// });
+const client = new OpenAI();
 
 const router = express.Router();
 
 router.get('/models', (req, res) => {
-    axios.get(`${OPENAI_URL}models`, {
+    axios.get(`${OPENAI_URL}/models`, {
         headers: {
           Authorization: `Bearer ${OPENAI_API_KEY}`,
           'OpenAI-Organization': OPENAI_ORGANIZATION,
@@ -23,6 +29,35 @@ router.get('/models', (req, res) => {
         // console.log(result);
         res.json({ data: result.data.data });
       }).catch((err) => console.log(err));
+});
+
+router.get('/prompt', async (req, res) => {
+  try {
+    // Define the parameters for the chat completion
+    // const params: OpenAI.Chat.ChatCompletionCreateParams = {
+    //   messages: [{ role: 'user', content: 'Say this is a test' }],
+    //   model: 'davinci-002',
+    // };
+    // // Create a chat completion
+    // const chatCompletion: OpenAI.Chat.ChatCompletion = await client.chat.completions.create(params);
+    // console.log(chatCompletion.choices[0].message);
+    // res.json({ data: chatCompletion.choices[0].message });
+
+
+    //////////////////////////////////////
+    const completion = await client.chat.completions.create({
+        model: "davinci-002",
+        messages: [
+            {"role": "user", "content": "write a haiku about ai"}
+        ],
+        max_tokens: 150
+    });
+    console.log(completion.choices[0].message);
+    res.json({ data: completion.choices[0].message });
+
+  } catch (error) {
+    console.error('Error:', error);
+  }
 });
 
 export default router;
